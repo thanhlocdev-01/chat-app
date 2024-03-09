9314aea78faffa4e684212619fedeac92c545019
ThanhLoc <thanhloclol6@gmail.com>
2024-03-09 14:10:56 +0700

    update deploy

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,14 @@ const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 const path = require("path");
 
-const connectMongoDB = require("./db/connectMongoDB.js");
 const authRoutes = require("./routes/auth.routes");
 const messageRoutes = require("./routes/message.routes");
 const userRoutes = require("./routes/user.routes");
+
+const connectMongoDB = require("./db/connectMongoDB.js");
 const { app, server } = require("./socket/socket.js");
+
+const {} = require("../");
 
 const PORT = process.env.PORT || 5000;
 
@@ -20,10 +23,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
